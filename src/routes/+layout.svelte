da882d331d728d4d01529e68cd0f88f9574bf4c0
Jahirul Islam <jahir.ewubd@gmail.com>
2025-04-06 01:33:25 +0200

    Implement breadcrumb navigation in layout; add generateBreadCrumbs function for dynamic URL paths

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,6 +6,17 @@
 	function toTitleCase(str: string) {
 		return str.replace(/\w\S*/g, (txt) => txt.charAt(0).toUpperCase() + txt.slice(1).toLowerCase());
 	}
+
+	function generateBreadCrumbs(path: string): Array<{ path: string; url: string }> {
+		// path/sub/anotherpath
+		// [{path: 'path', url: '/path'}, {path: 'sub', url: '/path/sub'}]
+		const pathArray = path.split('/').filter((p) => p !== '');
+		let urlSoFar = '';
+		return pathArray.map((p) => {
+			urlSoFar += `/${p}`;
+			return { path: p, url: urlSoFar };
+		});
+	}
 </script>
 
 <div class="min-h-screen bg-gray-50">
@@ -15,7 +26,9 @@
 				<a href="/" class="hover:text-gray-900">Home</a>
 				{#if page.url.pathname !== '/'}
 					<span>/</span>
-					<span class="text-gray-900">{toTitleCase(page.url.pathname.substring(1))}</span>
+					{#each generateBreadCrumbs(page.url.pathname.substring(1)) as urlpath}
+						<a href={urlpath.url} class="hover:text-gray-900">{toTitleCase(urlpath.path)} /</a>
+					{/each}
 				{/if}
 			</div>
 		</div>
