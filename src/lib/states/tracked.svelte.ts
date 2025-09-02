893296c827bf7351f082a1c16afb42bf99b30f71
Jahirul Islam <jahir.ewubd@gmail.com>
2025-09-02 23:13:05 +0200

    Rename the /user api to /me

--- src/lib/states/tracked.svelte.ts
+++ src/lib/states/tracked.svelte.ts
@@ -25,7 +25,7 @@ class TrackedProducts {
     }
 
     refresh() {
-        return fetch('/api/user/products')
+        return fetch('/api/me/products')
         .then((response) => {
             if (!response.ok) {
                 if (response.status === 401) {
