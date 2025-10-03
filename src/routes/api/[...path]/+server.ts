6d1f7bfe417ff8ce05c3e05157c05c4186c9d4af
Jahirul Islam <jahir.ewubd@gmail.com>
2025-10-03 02:29:08 +0200

    Fix not passing the full request in fallback api

--- src/routes/api/[...path]/+server.ts
+++ src/routes/api/[...path]/+server.ts
@@ -1,13 +1,14 @@
 import { API_URL } from '$env/static/private'
 import type { RequestEvent } from "@sveltejs/kit";
 
-export function fallback(req: RequestEvent) {
-    console.log('Fallback for', req.url.href);
+export function fallback({request, fetch, url}: RequestEvent) {
+    console.log('Fallback for', request.method, request.url, url.origin);
     const apiRequest = new Request(
-        req.url.href.replace(
-            `${req.url.origin}/api/`,
+        request.url.replace(
+            `${url.origin}/api/`,
             `${API_URL}/api/`
-        )
+        ),
+        request,
     );
-    return req.fetch(apiRequest);
+    return fetch(apiRequest);
 }
